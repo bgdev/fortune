c8dc5028743c4aae00152bb3f018fde542004cfc
Yordan Miladinov <yordan.miladinov@chaosgroup.com>
2016-01-13 14:42:12 +0000

    initial #2

--- fortune.js
+++ fortune.js
@@ -1,9 +1,35 @@
 /*jslint browser */
 
-(function (window, undefined) {
-    var url = "",
-	fortune = function () {
-	    
-	};
+(function () {
+    'use strict';
 
-}(window));
+    var id = 'logostrip',
+        url = 'https://raw.githubusercontent.com/bgdev/fortune/master/cookies',
+
+        handler = function (id) {
+            if (this.status === 200) {
+                var lines = this.responseText.trim().split('\n');
+                var rnd = Math.floor(Math.random() * lines.length);
+                var element = document.getElementById(id);
+                element.innerHTML = lines[rnd];
+            }
+        },
+
+        fortune = function (id, url) {
+            var req = new XMLHttpRequest();
+            req.addEventListener('load', function () {
+                handler.call(this, id);
+            });
+            req.open('GET', url);
+            req.send();
+        };
+
+    fortune(id, url);
+
+    // Polyfill String::trim()
+    if (!String.prototype.trim) {
+        String.prototype.trim = function () {
+            return this.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, '');
+        };
+    }
+}());
